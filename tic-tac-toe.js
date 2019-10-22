c5fa1c68e56e34b27cd3439dcb0bf25b7d53aeca
Tiffany Garrick <tiffygg@gmail.com>
2019-10-22 00:48:55 -0400

    Created board for game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,15 @@
-function makeBlocks() {
-	document.getElementById("board").classList.add("square");
-	document.getElementById("board").onclick=makeBlocks();
+window.onload= function() =>{
+	var elem=document.getElementById("board");
+	var elemArray=elem.querySelectorAll("div");
+	var i;
+	for (i=0; i < elemArray.length; i++) {
+		let box = elemArray[i];
+		box.classList.add("square");
+		box.onmouseover = function(){
+            box.classList.add('hover');
+        }
+        box.onmouseleave = function(){
+            box.classList.remove('hover');
+        }
+	}
 }
